9002d240ad3b036f93caf6b1e32e09242b767ea0
Cbameron12 <129990890+Cbameron12@users.noreply.github.com>
2025-01-17 11:25:44 +0000

    CI implementation (#8)
    * Added files
    
    * Added workflows
    
    * Added example test
    
    * Added janus_web init
    
    * Added .gitignore

---
+++ tests/test_example.py
@@ -0,0 +1,11 @@
+"""Example test."""
+
+from __future__ import annotations
+
+from janus_web import __version__
+
+
+def test_example():
+    """Example test."""
+    print(__version__)
+    assert 1 == (2 / 2)
