ba6fea11119de14d119b7d088f7ee42f1b6c0364
Cameron <cameron.ball@stfc.ac.uk>
2025-01-24 11:14:52 +0000

    Added numpy converter to spoint helper

--- api/utils/singlepoint_helper.py
+++ api/utils/singlepoint_helper.py
@@ -2,11 +2,37 @@
 
 from __future__ import annotations
 
+import logging
 from pathlib import Path
 from typing import Any
 
 from janus_core.calculations.single_point import SinglePoint
 import numpy as np
+
+logger = logging.getLogger(__name__)
+
+
+def convert_ndarray_to_list(obj) -> dict:
+    """
+    Recursive function to convert numpy arrays into a useable format for fastAPI.
+
+    Parameters
+    ----------
+    obj : Any
+        The object to be checked and potentially converted.
+
+    Returns
+    -------
+    dict
+        Dictionary of properties calculated.
+    """
+    if isinstance(obj, np.ndarray):
+        return obj.tolist()
+    if isinstance(obj, dict):
+        return {k: convert_ndarray_to_list(v) for k, v in obj.items()}
+    if isinstance(obj, list):
+        return [convert_ndarray_to_list(i) for i in obj]
+    return obj
 
 
 def singlepoint(
@@ -45,26 +71,19 @@ def singlepoint(
         "device": "cpu",
         "read_kwargs": read_kwargs,
     }
-    print(singlepoint_kwargs)
+    logger.info(singlepoint_kwargs)
 
     s_point = SinglePoint(**singlepoint_kwargs)
 
     s_point.run()
 
-    results = s_point.results
-
-    # Convert numpy arrays to lists
-    for key, value in results.items():
-        if isinstance(value, np.ndarray):
-            results[key] = value.tolist()
-
-    return results
+    return convert_ndarray_to_list(s_point.results)
 
 
 if __name__ == "__main__":
     results = singlepoint(
-        struct=Path("janus-web/data/input.data.2055.xyz"),
-        range_selector="0:1",
-        properties=["stress"],
+        struct=Path("/home/ubuntu/janus-api/janus-web/data/input.data.2055.xyz"),
+        range_selector="0:2",
     )
     print(results)
+    print(type(results))
